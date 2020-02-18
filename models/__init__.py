c3d68b07caf62a6385d0015eedd282d8bcb458ff
pablo <1283@holbertonschool.com>
2020-02-17 23:34:35 -0500

    fix pep8 classes variable

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@
 
 from models.engine.file_storage import FileStorage
 
-classes = {'BaseModel': BaseModel, 'Amenity': Amenity, 'State': State,
-        'Place': Place, 'Review': Review, 'User': User}
+classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
+           'Place': 'Place', 'Review': 'Review', 'User': 'User'}
 storage = FileStorage()
 storage.reload()
