ab6150704205590ffd774bee8706dd70701cb605
pablo <1283@holbertonschool.com>
2020-02-17 23:35:40 -0500

    import all classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,12 +5,11 @@ file to instances:
 
 import json
 from models.base_model import BaseModel
-# from models.amenity import Amenity
-# from models.city import City
-# from models.place import Place
-# from models.review import Review
-# from models.state import State
-# from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage():
