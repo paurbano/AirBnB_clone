d71f8806deab373eb243892882a4ffce0161b3e0
paurbano <paurbano@gmail.com>
2020-02-18 18:37:35 +0000

    connection layer

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ file to instances:
 import json
 from models.base_model import BaseModel
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
