f5cd5f9bf6f2b65b37e04aaf0497e49e7d14e71f
paurbano <paurbano@gmail.com>
2020-02-19 13:14:52 +0000

    delete call init super

--- models/user.py
+++ models/user.py
@@ -11,6 +11,6 @@ class User(BaseModel):
     first_name = ""
     last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """ Constructor """
-        super().__init__(self, *args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     """ Constructor """
+    #     super().__init__(self, *args, **kwargs)
